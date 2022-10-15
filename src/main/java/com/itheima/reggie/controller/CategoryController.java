d9b0331d4ef9da5245ac04568d29fed510b9789b
xingqian5 <114239048+xingqian5@users.noreply.github.com>
2022-10-15 21:45:06 +0800

    Update CategoryController.java

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -26,7 +26,7 @@ public class CategoryController {
         System.out.println("91");
         System.out.println("6");
         System.out.println("1");
-        System.out.println("2");
+        System.out.println("3");
         return R.success("新增分类成功1");
         
     }
